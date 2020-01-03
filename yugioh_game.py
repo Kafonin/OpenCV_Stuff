69110a6ed26d7fa43b733dc8d0d0becb62b7762e
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2020-01-03 00:30:41 -0500

    Update yugioh_game.py

--- yugioh_game.py
+++ yugioh_game.py
@@ -8,6 +8,8 @@ import send2trash
 #####################################################################################################
 #####################################################################################################
 
+
+
 def main():
     cam = cv2.VideoCapture(0)
     cv2.namedWindow("Test_Window")
@@ -33,16 +35,17 @@ def main():
 
     cam.release()
     cv2.destroyAllWindows()
-    im = Image.open("sample0.png")
-    text = pytesseract.image_to_string(im, lang = 'eng')
+    #im = Image.open("sample0.png")
+    #text = pytesseract.image_to_string(im, lang = 'eng')
 
-    if "BLUE EYES WHITE DRAGON" or "Blue Eyes White Dragon"  in text:
-        print("Place holder text -- read success.")
-        send2trash.send2trash("sample0.png")
+    #if "BLUE EYES WHITE DRAGON" or "Blue Eyes White Dragon"  in text:
+    #    print("Place holder text -- read success.")
+    #    send2trash.send2trash("sample0.png")
+    #    return 1
 
-    if "DARK MAGICIAN" or "Dark Magician"  in text:
-        print("Place holder text -- read success.")
-        send2trash.send2trash("sample0.png")
+    #if "DARK MAGICIAN" or "Dark Magician"  in text:
+    #    print("Place holder text -- read success.")
+    #    send2trash.send2trash("sample0.png")
 
 #####################################################################################################
 #####################################################################################################
@@ -52,15 +55,15 @@ def game():
     # initialize the pygame module
     pygame.init()
     
-    window_width=1920
-    window_height=1080
+    window_width=1280
+    window_height=745
 
     animation_increment=10
     clock_tick_rate=20
 
-    #open a window
+    # open a window
     size = (window_width, window_height)
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(size) # screen size here
 
     # Set title to the window
     pygame.display.set_caption("It's time to duel!")
@@ -69,22 +72,39 @@ def game():
 
     clock = pygame.time.Clock()
     background_image = pygame.image.load("bg.jpg").convert()
-    background_image = pygame.transform.scale(background_image, (1920, 1080))
+    background_image = pygame.transform.scale(background_image, (1280, 745))
+    duelMonsterOne = pygame.image.load("be.jpg")
+    duelMonsterTwo = pygame.image.load("dm.jpg")
 
     while(dead==False):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 dead = True
 
-        screen.blit(background_image, [0, 0])
-
         pygame.display.flip()
         clock.tick(clock_tick_rate)
         main()
+        im = Image.open("sample0.png")
+        text = pytesseract.image_to_string(im, lang = 'eng')
+        screen.blit(background_image, [0, 0])
+
+        if "BLUE EYES WHITE DRAGON" or "Blue Eyes White Dragon"  in text:
+            #print("Place holder text -- read success.")
+            send2trash.send2trash("sample0.png")
+            screen.blit(duelMonsterOne, (500, 100))
+            pygame.display.update()
+            clock.tick(clock_tick_rate)
+
+        if "DARK MAGICIAN" or "Dark Magician"  in text:
+            #print("Place holder text -- read success.")
+            send2trash.send2trash("sample0.png")
+            screen.blit(duelMonsterTwo, (500, 500))
+            pygame.display.update()
+            clock.tick(clock_tick_rate)
 
 #####################################################################################################
 #####################################################################################################
 
 if __name__=="__main__":
     # call the main function
-    game()    
+    theGame = game()
